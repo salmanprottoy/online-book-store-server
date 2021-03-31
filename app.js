e48f9db811f5dbae3352fc4b9b1f5f6aa4d3a174
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2021-03-31 18:16:49 +0600

    updated server

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yorqu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const booksCollection = client.db("onlineBookStore").collection("books");
+  const ordersCollection = client.db("onlineBookStore").collection("orders");
+
+  app.post("/addBook", (req, res) => {
+    const books = req.body;
+    booksCollection.insertOne(books).then((result) => {
+      console.log(result);
+      res.send(result);
+    });
+  });
+
+  app.get("/books", (req, res) => {
+    booksCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.get("/book/:key", (req, res) => {
+    booksCollection.find({ key: req.params.key }).toArray((err, documents) => {
+      res.send(documents[0]);
+    });
+  });
+
+  app.post("/booksByKeys", (req, res) => {
+    const bookKeys = req.body;
+    booksCollection
+      .find({ key: { $in: bookKeys } })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order).then((result) => {
+      console.log(result);
+      res.send(result);
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Node app started at port ${port}`);
+});
